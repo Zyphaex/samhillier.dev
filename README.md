86af5516ee0e1a2b31541f88b7232dc0a9998d17
Sam Hillier <sam@hillier.uk>
2023-08-13 09:06:45 +0100

    samhillier.dev

---
+++ README.md
@@ -0,0 +1,67 @@
+# Hi, I'm Sam :wave:
+
+`Full-stack`, results-oriented `Computer Science` graduate with a proven record of academic and project success, seeking to apply
+programming skills and a collaborative approach to contribute to organisational excellence in system health, scalability, and longevity.
+
+If you're interested in working with me or have a question, please feel free to get in touch.
+
+- Check out my [portfolio website](https://samhillier.dev/) ✍
+- Lives in United Kingdom 🇬🇧
+
+[![GitHub Sponsor](https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86)](https://github.com/sponsors/Zyphaex)
+
+## 🤝 Let's Connect
+[![LinkedIn](https://skillicons.dev/icons?i=linkedin)](https://linkedin.com/in/samhillier/)
+[![X](https://skillicons.dev/icons?i=twitter)](https://x.com/Zyphaex/)
+[![Instagram](https://skillicons.dev/icons?i=instagram)](https://instagram.com/Zyphaex/)
+[![Discord](https://skillicons.dev/icons?i=discord)](https://discord.com/invite/XfZHVfPr9C)
+
+## 💻 Languages
+[![Languages](https://skillicons.dev/icons?i=js,ts,py,java,cpp,html,css)](#)
+
+## 🏗️ Frameworks
+[![Frameworks](https://skillicons.dev/icons?i=react,nodejs,express,vue)](#)
+
+## 🛠️ Tools
+[![Tools](https://skillicons.dev/icons?i=aws,azure,docker,git,postgresql,mysql,tailwind)](#)
+
+## 🏆 GitHub Stats
+
+| ![Sam Hillier's GitHub Stats](https://github-readme-stats.vercel.app/api?username=Zyphaex&show_icons=true&theme=tokyonight&count_private=true&hide_border=true&hide=contribs,issues&include_all_commits=true&line_height=38) | ![Sam's GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=Zyphaex&theme=tokyonight&hide_border=true) |
+| --- | --- |
+
+## Requirements
+
+Make sure you have Node and NPM installed:
+
+```bash
+node -v
+# v22.10.0
+```
+```bash
+npm -v
+# 10.9.0
+```
+
+## Installation
+
+Run the commands:
+```bash
+npm install
+```
+```bash
+npm run dev
+```
+
+## Security Vulnerabilities
+
+Please review our [security policy](https://github.com/Zyphaex/samhillier.dev/security/policy) on how to report security vulnerabilities.
+
+## Credits
+
+- [Sam Hillier](https://github.com/Zyphaex)
+- [All Contributors](https://github.com/Zyphaex/samhillier.dev/contributors)
+
+## License
+
+Please see [License File](https://github.com/Zyphaex/samhillier.dev/blob/main/LICENSE) for more information.
---
+++ src/App.tsx
@@ -0,0 +1,164 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Sam Hillier</h1>
+        <h2>Software Engineer</h2>
+      </header>
+      <main>
+        <section>
+          <ul>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C++</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </ul>
+        </section>
+        <section>
+          <h3>About Me</h3>
+          <p>
+            I'm a Computer Science graduate from Nottingham Trent University, a
+            Full-Stack Web Developer and Software Engineer with a proven record
+            of academic and project success.
+          </p>
+          <p>
+            If you're interested in working with me or have a question, please
+            feel free to get in touch.
+          </p>
+          <img
+            src="https://raw.githubusercontent.com/Zyphaex/samhillier.dev/main/src/assets/images/sam-hillier.jpg"
+            width="200"
+            height="200"
+            alt="Sam Hillier"
+          ></img>
+        </section>
+        <section>
+          <h3>Projects</h3>
+          <article>
+            <a
+              href="https://cs2.team"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Visit CS2.TEAM website"
+            >
+              <h4>CS2.TEAM</h4>
+            </a>
+            <p>
+              Find esports teams and players on CS2.TEAM.
+            </p>
+          </article>
+          <article>
+            <a
+              href="https://enjindex.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Visit ENJindex website"
+            >
+              <h4>ENJindex</h4>
+            </a>
+            <p>ENJindex, explore NFTs and crypto collectibles.</p>
+          </article>
+        </section>
+        <section>
+          <h3>Certifications</h3>
+          <a
+            href="https://credly.com/badges/cd51660c-b4b3-4348-8474-6111af2d9f59"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h4>AWS Certified AI Practitioner</h4>
+            <img
+              src="https://raw.githubusercontent.com/Zyphaex/samhillier.dev/main/src/assets/images/certifications/aws-ai-practitioner-foundational.png"
+              alt="AWS Certified AI Practitioner"
+              width="100"
+              height="100"
+            ></img>
+          </a>
+          <a
+            href="https://credly.com/badges/a4984764-8bc9-44e6-914a-c7c65767c9e1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h4>AWS Certified Cloud Practitioner</h4>
+            <img
+              src="https://raw.githubusercontent.com/Zyphaex/samhillier.dev/main/src/assets/images/certifications/aws-cloud-practitioner-foundational.png"
+              alt="AWS Certified Cloud Practitioner"
+              width="100"
+              height="100"
+            ></img>
+          </a>
+        </section>
+      </main>
+      <footer>
+        <section>
+          <a
+            href="https://github.com/Zyphaex"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub profile"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://linkedin.com/in/samhillier"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn profile"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://x.com/Zyphaex"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="X profile"
+          >
+            X
+          </a>
+          <a
+            href="https://instagram.com/Zyphaex"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram profile"
+          >
+            Instagram
+          </a>
+        </section>
+        <section>
+          <iframe
+            src="https://github.com/sponsors/Zyphaex/button"
+            id="sponsor"
+            title="GitHub Sponsor"
+            height="32"
+            width="114"
+            style={{ border: 0, borderRadius: "6px" }}
+          ></iframe>
+        </section>
+        <section>
+          <nav>
+            <a href="#" aria-label="About page">
+              About
+            </a>
+            <a href="#" aria-label="Contact page">
+              Contact
+            </a>
+            <a href="#" aria-label="Terms and Conditions page">
+              Terms & Conditions
+            </a>
+            <a href="#" aria-label="Privacy Policy page">
+              Privacy Policy
+            </a>
+          </nav>
+          <p>© 2024 Sam Hillier. All Rights Reserved.</p>
+        </section>
+      </footer>
+    </>
+  );
+}
+
+export default App;
